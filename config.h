4bebf56b1d97720ed14e6403d476b389968c646b
kiddion <895118352@qq.com>
2022-07-09 02:21:25 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,14 +1,5 @@
-/*
- * XXX: put this file under license if needed
- *
- */
-
- /** @file config-gcc.h
- * Build configuration for gcc platform.
- *
- * Based on auto-generated config.h. Should be manually synced whenever
- * additional configuration parameters are added.
- */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Use additional validation checks */
 #define CHECK 1
@@ -16,15 +7,20 @@
 /* Journal file directory */
 #define DBLOG_DIR "/tmp"
 
-/* Select locking protocol (undef to disable locking)
- * 1 - reader preference spinlock
- * 2 - writer preference spinlock
- * 3 - task-fair queued lock
- */
-#define LOCK_PROTO 1
-
 /* Encoded data is 64-bit */
-/* #undef HAVE_64BIT_GINT */
+#define HAVE_64BIT_GINT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
 /* Define if you have POSIX threads libraries and header files. */
 #define HAVE_PTHREAD 1
@@ -32,8 +28,32 @@
 /* Compile with raptor rdf library */
 /* #undef HAVE_RAPTOR */
 
-/* Define to 1 if your C compiler doesn't accept -c and -o together. */
-/* #undef NO_MINUS_C_MINUS_O */
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Select locking protocol: reader-preference spinlock */
+#define LOCK_PROTO 3
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
 
 /* Name of package */
 #define PACKAGE "whitedb"
@@ -50,12 +70,21 @@
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "whitedb"
 
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.8-alpha"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
 /* #undef PTHREAD_CREATE_JOINABLE */
+
+/* The size of `ptrdiff_t', as computed by sizeof. */
+#define SIZEOF_PTRDIFF_T 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
 
 /* String hash size (% of db size) */
 #define STRHASH_SIZE 2
@@ -75,11 +104,11 @@
 /* Use dblog module for transaction logging */
 /* #undef USE_DBLOG */
 
-/* Use match templates for indexes */
-#define USE_INDEX_TEMPLATE 1
-
 /* Enable runtime diagnostics via error callback */
 #define USE_ERROR_CALLBACK 1
+
+/* Use match templates for indexes */
+#define USE_INDEX_TEMPLATE 1
 
 /* Enable reasoner */
 /* #undef USE_REASONER */
@@ -95,3 +124,7 @@
 
 /* Package revision number */
 #define VERSION_REV 0
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+#define YYTEXT_POINTER 1
